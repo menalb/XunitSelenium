aa9a14a86d4bc805ec7c61db5131556b9414a417
menalb1984 <menalb1984@live.com>
2016-07-17 00:04:14 +0100

    added selenium tests

--- XunitSeleniumTests/Tests.cs
+++ XunitSeleniumTests/Tests.cs
@@ -1,8 +1,13 @@
-﻿using Xunit;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+using System;
+using Xunit;
 
 namespace XunitSeleniumTests
 {
-    public class Tests
+
+    public class UnitTests
     {
         [Fact]
         public void Everything_Is_OK()
@@ -14,6 +19,39 @@ namespace XunitSeleniumTests
         public void OPS()
         {
             Assert.True(0 == 1);
+        }
+    }
+
+    public class SeleniumTests : IDisposable
+    {
+        private readonly RemoteWebDriver _driver;
+
+        public SeleniumTests()
+        {
+            var capability = DesiredCapabilities.Chrome();
+            _driver = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub"), capability);
+        }
+
+        public void Dispose()
+        {
+            _driver.Quit();
+            _driver.Dispose();
+        }
+
+        [Fact]
+        public void SeleniumTest()
+        {
+            _driver.Url = "http://www.google.com";
+            _driver.Navigate();
+
+            IWebElement element = _driver.FindElement(By.Name("q"));
+            element.SendKeys("Cheese!");
+            element.Submit();
+
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            IWebElement myDynamicElement = wait.Until<IWebElement>(d => d.FindElement(By.Id("resultStats")));
+
+            Assert.Contains("Cheese", _driver.Title);
         }
     }
 }
