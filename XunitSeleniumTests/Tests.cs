8a192ab66110169725f0afeac64307acdd584f3b
menalb1984 <menalb1984@live.com>
2016-07-16 20:38:23 +0100

    initial commit

---
+++ XunitSeleniumTests/Tests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace XunitSeleniumTests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Everything_Is_OK()
+        {
+            Assert.True(1 == 1);
+        }
+
+        [Fact]
+        public void OPS()
+        {
+            Assert.True(0 == 1);
+        }
+    }
+}
